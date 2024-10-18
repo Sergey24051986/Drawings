9d262d37e76017160cb32bd0c1604e863ffa6a55
Sergey <serjio.nef@gmail.com>
2024-10-18 14:15:32 +0300

    Update TrafficLights

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -3,11 +3,14 @@ import java.awt.*;
 
 public class TrafficLights extends Canvas
 {
-    public void paint(Graphics g) {
-        setBackground(Color.GRAY);// отрисовка фона
+    public void paint(Graphics g0) {
+        Graphics2D g = (Graphics2D)g0;
+        g.setBackground(Color.GRAY);// отрисовка фона
+        g.clearRect(0,0,800,800);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);// Сглаживание
         g.setColor(Color.black);
         g.fillRoundRect(320, 140, 160, 410, 50, 50);// Корпус светофора
-        g.fillArc(350, 116, 100, 20, 25, 130);// Шляпа верх
+        g.fillArc(350, 116, 100, 20, 18, 160);// Шляпа верх
         g.fillRoundRect(350, 123, 100, 10, 10, 10);// Шляпа низ
         g.fillRoundRect(230, 170, 80, 80, 10, 10);// Левый верхний треугольник (л.в.)
         g.fillRoundRect(230, 300, 80, 80, 10, 10);// Левый средний треугольник (л.с.)
@@ -37,6 +40,20 @@ public class TrafficLights extends Canvas
         g.fillPolygon(new int[]{580, 580, 485}, new int[]{165, 260, 260}, 3); // Треугольник убирающий часть (п.в.)прямоугольника
         g.fillPolygon(new int[]{580, 580, 485}, new int[]{295, 390, 390}, 3); // Треугольник убирающий часть (п.с.)прямоугольника
         g.fillPolygon(new int[]{580, 580, 485}, new int[]{425, 520, 520}, 3); // Треугольник убирающий часть (п.н.)прямоугольника
+        // Контуры
+        g.setColor(Color.black);
+        g.drawRoundRect(320, 140, 160, 410, 50, 50);// Корпус светофора
+        g.drawArc(350, 116, 100, 20, 25, 130);// Шляпа верх
+        g.drawRoundRect(350, 123, 100, 10, 10, 10);// Шляпа низ
+        g.drawArc(344, 150, 111, 130, 0, 180);// Верхний козырек(верхний полукруг)
+        g.drawArc(344, 280, 111, 130, 0, 180);// Средний козырек(верхний полукруг)
+        g.drawArc(344, 410, 111, 130, 0, 180);// Нижний козырек(верхний полукруг)
+        g.drawArc(345, 162, 110, 115, 0, 180); // Верхний козырек(нижний полукруг)
+        g.drawArc(345, 292, 110, 115, 0, 180); // Средний козырек(нижний полукруг)
+        g.drawArc(345, 422, 110, 115, 0, 180); // Нижний козырек(нижний полукруг)
+        g.drawOval(350, 170, 100, 100); // Красный круг
+        g.drawOval(350, 300, 100, 100); // Желтый круг
+        g.drawOval(350, 430, 100, 100); // Зеленый круг
     }
 
     public static void main(String[] args) {
